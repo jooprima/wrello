09b3437e96a22bfc67db46bc0ec0831419bc9f44
jooprima <jooprima@gmail.com>
2021-01-04 01:19:59 +0700

    Enable the Draggable on TaskCard Component

--- src/components/Kanban/TaskCard/index.tsx
+++ src/components/Kanban/TaskCard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 import { Box, Text, Flex } from "@chakra-ui/react";
 
 type TaskCardProps = {
@@ -7,24 +8,36 @@ type TaskCardProps = {
   content: string;
 };
 
-const TaskCard: React.FC<TaskCardProps> = ({ content }) => {
+const TaskCard: React.FC<TaskCardProps> = ({ content, id, index }) => {
   return (
-    <Box width="100%" bg="white" p={1} rounded={1}>
-      <Flex alignItems="center">
-        <Text
-          mr={4}
-          flex={1}
-          fontSize="sm"
-          wordBreak="break-all"
-          whiteSpace="normal"
-          color="gray.900"
+    <Draggable draggableId={id} index={index}>
+      {(provided, snapshot) => (
+        <Box
+          width="100%"
+          bg="white"
+          p={1}
+          rounded={2}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
         >
-          {content}
-        </Text>
+          <Flex alignItems="center">
+            <Text
+              mr={4}
+              flex={1}
+              fontSize="sm"
+              wordBreak="break-all"
+              whiteSpace="normal"
+              color="gray.900"
+            >
+              {content}
+            </Text>
 
-        <span>=</span>
-      </Flex>
-    </Box>
+            <span>=</span>
+          </Flex>
+        </Box>
+      )}
+    </Draggable>
   );
 };
 
