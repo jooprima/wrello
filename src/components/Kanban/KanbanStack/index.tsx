ba6ba213bfdd897a2bd2b942314973293bf7a81f
jooprima <jooprima@gmail.com>
2021-01-03 16:55:36 +0700

    enable the DragDropContext on KanbanStack Component

--- src/components/Kanban/KanbanStack/index.tsx
+++ src/components/Kanban/KanbanStack/index.tsx
@@ -1,29 +1,39 @@
-import React from "react";
-import {  HStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { HStack } from "@chakra-ui/react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 import KanbanCard from "components/Kanban/KanbanCard";
+import { initialData } from "data/initial-data";
 
 const KanbanStack: React.FC = () => {
+  const [data, setData] = useState(initialData);
+
+  const onDragEnd = (result: DropResult): void => {
+    //akan kita bahas di part selanhutnya
+  };
+
   return (
-    <HStack
-      mt="40px"
-      p={4}
-      spacing={8}
-      top={0}
-      right={0}
-      bottom={0}
-      left={0}
-      userSelect="none"
-      whiteSpace="nowrap"
-      overflowX="auto"
-      overflowY="hidden"
-      position="absolute"
-      alignItems="flex-start"
-    >
-      <KanbanCard title="KanbanCard1" id="task-1" tasks={[]} />
-      <KanbanCard title="KanbanCard2" id="task-2" tasks={[]} />
-      <KanbanCard title="KanbanCard3" id="task-3" tasks={[]} />
-    </HStack>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <HStack
+        mt="40px"
+        p={4}
+        spacing={8}
+        top={0}
+        right={0}
+        bottom={0}
+        left={0}
+        userSelect="none"
+        whiteSpace="nowrap"
+        overflowX="auto"
+        overflowY="hidden"
+        position="absolute"
+        alignItems="flex-start"
+      >
+        <KanbanCard title="KanbanCard1" id="task-1" tasks={[]} />
+        <KanbanCard title="KanbanCard2" id="task-2" tasks={[]} />
+        <KanbanCard title="KanbanCard3" id="task-3" tasks={[]} />
+      </HStack>
+    </DragDropContext>
   );
 };
 
